9a7029e7d9b687077b92edfc84315f8c41bd7008
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 04:57:38 +0300

    Update LoginActivity.kt

--- activity/LoginActivity.kt
+++ activity/LoginActivity.kt
@@ -1,18 +1,3 @@
-package com.yucox.splitwise.activity
-
-
-import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import android.view.View
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseAuth
-import com.yucox.splitwise.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
