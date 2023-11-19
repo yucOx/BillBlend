982cb165fb1a6a6bb7ec1ee0dda1e3ee18206a31
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 05:00:27 +0300

    Update AddFriendFragment.kt

--- fragment/AddFriendFragment.kt
+++ fragment/AddFriendFragment.kt
@@ -1,27 +1,3 @@
-package com.yucox.splitwise.fragment
-
-import android.os.Bundle
-import android.os.Handler
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
-import androidx.core.widget.addTextChangedListener
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.FirebaseStorage
-import com.R.R.model.UserInfo
-import com.yucox.splitwise.R
-import com.yucox.splitwise.adapter.SearchAdapter
 
 
 class AddFriendFragment : Fragment() {
