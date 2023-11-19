08409dd1584f6e23f671715b7e31f6efad2c0521
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 04:56:44 +0300

    Update CreateGroup.kt

--- activity/CreateGroup.kt
+++ activity/CreateGroup.kt
@@ -1,34 +1,3 @@
-package com.yucox.splitwise.activity
-
-
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.MobileAds
-import com.google.android.material.textfield.TextInputLayout
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.R.R.model.GetMails
-import com.R.R.model.SendFriendRequest
-import com.R.R.model.Group
-import com.yucox.splitwise.R
 
 class CreateGroup : AppCompatActivity() {
     private lateinit var database : FirebaseDatabase
