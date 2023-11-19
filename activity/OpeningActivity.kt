61d745e1e77d0bebb722af65d0488b462398c162
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 04:58:13 +0300

    Update OpeningActivity.kt

--- activity/OpeningActivity.kt
+++ activity/OpeningActivity.kt
@@ -1,12 +1,3 @@
-package com.yucox.splitwise.activity
-
-
-
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.Handler
-import com.yucox.splitwise.R
 
 class OpeningActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
