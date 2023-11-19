b02b47d78f6cd6efc34dd864fa87121ca5b994fe
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 04:59:24 +0300

    Update ListGroupAdapter.kt

--- adapter/ListGroupAdapter.kt
+++ adapter/ListGroupAdapter.kt
@@ -1,18 +1,3 @@
-package com.yucox.splitwise.adapter
-
-
-import android.content.Context
-import android.content.Intent
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import com.R.R.model.Group
-import com.yucox.splitwise.R
-import com.yucox.splitwise.activity.DetailsOfGroupActivity
-import de.hdodenhof.circleimageview.CircleImageView
 
 class ListGroupAdapter(private val context: Context, private var groupUsers: ArrayList<Group>, private var groupNames : HashSet<String>) :
     RecyclerView.Adapter<ListGroupAdapter.ViewHolder>() {
