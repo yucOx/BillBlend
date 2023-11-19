12b1f1eb61e4112b2d5ff2e95861a472eca99701
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 04:59:43 +0300

    Update SearchAdapter.kt

--- adapter/SearchAdapter.kt
+++ adapter/SearchAdapter.kt
@@ -1,28 +1,3 @@
-package com.yucox.splitwise.adapter
-
-import android.app.Activity
-import android.content.Context
-import android.os.Handler
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.R.R.model.SendFriendRequest
-import com.R.R.model.UserInfo
-import com.google.firebase.storage.ktx.storage
-import com.yucox.splitwise.R
-import de.hdodenhof.circleimageview.CircleImageView
 
 class SearchAdapter(private val context: Context, private var userList: ArrayList<UserInfo>, var randomImg : ArrayList<Int>) :
     RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
