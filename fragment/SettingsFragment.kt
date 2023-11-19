51b05a557e45b0b93c2c8f2b852b00a09be16ae9
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 05:00:36 +0300

    Update SettingsFragment.kt

--- fragment/SettingsFragment.kt
+++ fragment/SettingsFragment.kt
@@ -1,35 +1,3 @@
-package com.yucox.splitwise.fragment
-
-
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.os.Handler
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import com.yucox.splitwise.R
-import com.yucox.splitwise.activity.LoginActivity
-import de.hdodenhof.circleimageview.CircleImageView
-
 
 class SettingsFragment : Fragment() {
     private lateinit var currentUserInfo : com.R.R.model.UserInfo
