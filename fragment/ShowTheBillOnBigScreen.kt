a7d8c090cf533709008dea005b87a7a1a87abdb9
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 05:00:46 +0300

    Update ShowTheBillOnBigScreen.kt

--- fragment/ShowTheBillOnBigScreen.kt
+++ fragment/ShowTheBillOnBigScreen.kt
@@ -1,15 +1,3 @@
-package com.yucox.splitwise.fragment
-
-
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import com.yucox.splitwise.R
-
 
 class ShowTheBillOnBigScreen : Fragment() {
 
