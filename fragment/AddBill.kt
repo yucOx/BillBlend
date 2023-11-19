3c279083c029fa4fd62de7f2827c178737d3028c
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2023-11-19 05:00:10 +0300

    Update AddBill.kt

--- fragment/AddBill.kt
+++ fragment/AddBill.kt
@@ -1,39 +1,3 @@
-package com.yucox.splitwise.fragment
-
-import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.os.Handler
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ProgressBar
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.ktx.storage
-import com.R.R.model.Group
-import com.R.R.model.UserInfo
-import com.R.R.model.WhoHowmuch
-import com.yucox.splitwise.R
-
 
 class AddBill : Fragment() {
     private lateinit var database: FirebaseDatabase
