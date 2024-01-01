457d172c39e67921b3a572bb0d36e1e5366e508d
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2024-01-01 12:38:46 +0300

    Update AddBillActivity.kt

--- activity/AddBillActivity.kt
+++ activity/AddBillActivity.kt
@@ -773,7 +773,6 @@ class AddBillActivity : AppCompatActivity() {
         var adRequest = AdRequest.Builder().build()
         InterstitialAd.load(
             this,
-            "ca-app-pub-5841174734258930/8173377178",
             adRequest,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
