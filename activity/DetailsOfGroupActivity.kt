eb7132601fae5b2e84151f2b3e246c77cc515617
Mehmet Yücem Aydın <73720000+yucOx@users.noreply.github.com>
2024-02-23 10:38:10 +0300

    Update DetailsOfGroupActivity.kt

--- activity/DetailsOfGroupActivity.kt
+++ activity/DetailsOfGroupActivity.kt
@@ -506,7 +506,6 @@ class DetailsOfGroupActivity : AppCompatActivity() {
         var adRequest = AdRequest.Builder().build()
         InterstitialAd.load(
             this,
-            "ca-app-pub-5841174734258930/8173377178",
             adRequest,
             object : InterstitialAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
